8d62fc07a21658edf68a08d066ec53a9d5d47027
atikur-rabbi <msawon94@gmail.com>
2020-09-21 13:49:22 +0600

    Updated at 2020-09-21_0149 PM

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get("/", function(req, res) {
 
 
   app.get('/docs', function(req, res) {
-    var path = __dirname + '/markdown/readme.md';
+    var path = __dirname + '/markdown/test.md';
     fs.readFile(path, 'utf8', function(err, data) {
       if(err) {
         console.log(err);
